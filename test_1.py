45312eec968d87033f2dee42efa6afa3066b536b
user <sindulis99@gmail.com>
2018-10-01 17:46:41 +0300

    20181001_17_46

---
+++ test_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#http://www.cplusplus.com/reference/cstdio/printf
+
+a = 10
+b = 1.56
+c = 'B'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+a = 20
+b = 49.43478593
+c = 'B'
+print("a = %d"%(a))
+print("b = %.3f"%(b))
+print("c = %s"%(c))
+
