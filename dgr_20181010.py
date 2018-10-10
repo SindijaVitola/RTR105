2d88d245173838c2c1645504d9f32b4b62f69b68
user <sindulis99@gmail.com>
2018-10-10 13:37:07 +0300

    20181010_13_37

---
+++ dgr_20181010.py
@@ -0,0 +1,105 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print(456)
+456
+>>> print(56.7)
+56.7
+>>> print('Čau čau')
+Čau čau
+>>> x = 12.2
+>>> y = 14
+>>> x = 100
+>>> x*x
+10000
+>>> x1q3z9ocd = 35.0
+>>> x1q3z9afd = 12.50
+>>> x1q3p9afd = x1q3z9ocd*x1q3z9afd
+>>> print(x1q3p9afd)
+437.5
+>>> xx = 2
+>>> xx = xx+2
+>>> print(xx)
+4
+>>> yy = 440*12
+>>> print(yy)
+5280
+>>> zz = yy/10
+>>> print(zz)
+528
+>>> jj = 23
+>>> kk - jj%5
+
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    kk - jj%5
+NameError: name 'kk' is not defined
+>>> kk = jj % 5
+>>> print(kk)
+3
+>>> print(4**3)
+64
+>>> x = 1+2**3/4*5
+>>> print(x)
+11
+>>> ddd = 1+4
+>>> print(ddd)
+5
+>>> ddd = 56+84946
+>>> print(ddd)
+85002
+>>> 999 % 1000
+999
+>>> 1010 % 1000
+10
+>>> 23 % 5
+3
+>>> eee = 'čau'+'yo'
+>>> print(eee)
+čauyo
+>>> eee = 'čau' + 'yo'
+>>> print(eee)
+čauyo
+>>> eee = 'čau ' + 'yo'
+>>> print(eee)
+čau yo
+>>> type(eee)
+<type 'str'>
+>>> type(ddd)
+<type 'int'>
+>>> a = input("Ievadi veselu skaitli: ")
+Ievadi veselu skaitli: 7
+>>> type(a)
+<type 'int'>
+>>> a = ("Ievadi reālu skaitli: ")
+>>> a = input("Ievadi reālu skaitli: ")
+Ievadi reālu skaitli: 5.86
+>>> type(a)
+<type 'float'>
+>>> a = raw_input("Ievadi tekstu rindu: ")
+Ievadi tekstu rindu: ijsrbhgiubr
+>>> type(a)
+<type 'str'>
+>>> print(81/9)
+9
+>>> print(8537/3 % 3)
+1
+>>> nam  = input('Who are you?')
+Who are you?
+
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    nam  = input('Who are you?')
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+>>> print('Welcome', nam)
+
+Traceback (most recent call last):
+  File "<pyshell#52>", line 1, in <module>
+    print('Welcome', nam)
+NameError: name 'nam' is not defined
+>>> 
+
