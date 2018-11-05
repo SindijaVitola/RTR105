4929d1c0a06456efcd65477ae10b5ddd16640c3b
user <sindulis99@gmail.com>
2018-11-05 17:00:25 +0200

    20181105_17_00

---
+++ tests.py
@@ -0,0 +1,72 @@
+##str1 = "Nu"
+##str2 = "Čau"
+##fu = str1 + str2
+##print(fu)
+
+##name = input("Enter:")
+
+##3print(name)
+
+##fruit = "banana"
+##letter = fruit[1]
+##print(letter)
+
+##x = 3
+##w = fruit[x - 1]
+##print(w)
+
+##word = "Sindija"
+##x = len(word)
+##print(x)
+
+##word = "Sindija"
+##index = 0
+##while index < len(word):
+##    letter = word[index]
+##    print(index, letter)
+##    index = index + 1
+    
+word = "Ārprāc"
+count = 0
+for letter in word:
+    if letter == "ā":
+        count = count + 1
+##print(count)
+
+s = "Monty Python"
+##print(s[0:4])
+##print(s[6:7])
+##print(s[6:20])
+
+l = "Hello Yello"
+##print(l[0:5])
+##print(l[2:20])
+
+a = "Čau"
+b = a + "visiem"
+##print(b)
+c = a + " " + "visiem"
+##print(c)
+
+greet = "Hello Bob"
+zap = greet.lower()
+##print(zap)
+##print(greet)
+##print("Hi There".lower())
+
+lieta = "telefons"
+fr = lieta.find("lef")
+##print(fr)
+aa = lieta.find("m")
+##print(aa)
+
+greet = "Čau man"
+nstr = greet.replace("man","tev")
+##print(nstr)
+nstr = greet.replace("m","g")
+##print(nstr)
+
+line = "Lai tev jauka diena xoxo"
+print(line)
+
+
